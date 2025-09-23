af3a3556386803131b00d850e5d82729924ad5ff
9eplicant. <keizhatp@disroot.org>
2025-09-23 14:54:39 +0700

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+# Cherrypicked from cachyos repo, thanks for them
+
+source ~/.config/fish/still-config.fish
+source ~/.config/fish/conf.d/done.fish
+
+starship init fish | source
+
+# enable if needed
+#set -x QT_STYLE_OVERRIDE qt5ct
+#set -x QT_QPA_PLATFORMTHEME qt5ct
+
+#if status is-interactive
+#    # Commands to run in interactive sessions can go here
+#end
